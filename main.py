7fbf02791c41e9b044d3d91be152f3f981df8d96
root <root@localhost.localdomain>
2019-09-05 19:34:41 +0800

    3.8 轮廓检测

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 import requests
 import cv2
 import utils
-import numpy
+import numpy as np
 import filters
 import os
 from scipy import ndimage
 
+img = np.zeros((200,200),dtype=np.uint8)
+
+img[50:150,50:150] = 255
+
 src = cv2.imread('235157-106.jpg')
-cv2.namedWindow('myWindow1',cv2.WINDOW_NORMAL)
-cv2.namedWindow('myWindow2',cv2.WINDOW_NORMAL)
-cv2.namedWindow('myWindow3',cv2.WINDOW_NORMAL)
-cv2.imshow('myWindow1',src)
-#b,g,r = cv2.split(src)
-#
-#dtype = numpy.uint8
-#length = numpy.iinfo(dtype).max + 1
-#bFunc = None
-#vFunc = None
-#gFunc = None
-#rFunc = None
-#bLookupArray = utils.createLookupArray(utils.createCompositeFunc(bFunc, vFunc), length)
-#gLookupArray = utils.createLookupArray(utils.createCompositeFunc(gFunc, vFunc), length)
-#rLookupArray = utils.createLookupArray(utils.createCompositeFunc(rFunc, vFunc), length)
+ret, thresh = cv2.threshold(img,127,255,0)
+ret2,thresh2 = cv2.threshold(src,127,255,cv2.THRESH_BINARY)
+cv2.imshow('myWindow',thresh2)
+#findContours第一个是输入图像，第二个是轮廓检索模式，第三个是轮廓近似方法
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#utils.applyLookupArray(bLookupArray, b, b)
-#utils.applyLookupArray(gLookupArray, g, g)
-#utils.applyLookupArray(rLookupArray, r, r)
-
-#cv2.merge([b,g,r],src)
-#cv2.imshow('myWindow2',src)
-#cv2.waitKey(0)
-
-filters.strokeEdges(src,src)
-cv2.imshow('myWindow2',src)
-curveFilter = filters.BGRPortraCurveFilter()
-curveFilter.apply(src,src)
-cv2.imshow('myWindow3',src)
+color = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+#函数cv2.drawContours()被用来绘制轮廓。第一个参数是一张图片，可以是原图或者其他。第二个参数是轮廓，也可以说是cv2.findContours()找出来的点集，一个列表。第三个参数是对轮廓（第二个参数）的索引，当需要绘制独立轮廓时很有用，若要全部绘制可设为-1。接下来的参数是轮廓的颜色和厚度。
+img = cv2.drawContours(color,contours,-1,(0,255,0),2)
+cv2.imshow('contours',color)
 cv2.waitKey(0)
+cv2.destroyAllWindow()
